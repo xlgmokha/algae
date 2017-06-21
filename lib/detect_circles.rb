eb96a99de30f37a02d3540432e36477516b968b8
mo <mokha@cisco.com>
2017-06-21 14:06:23 -0600

    add next part to the problem.

--- lib/detect_circles.rb
+++ lib/detect_circles.rb
@@ -1,6 +1,10 @@
 <<-DOC
 You are given a single linked list, and you are asked to determine if there is a circle inside it
 A circle means, a -> b -> c -> b, then it is a circle
+
+Part 2.
+Could you get the length of the circle for the input single linked list if there is one, or output 0 if there is no circle? (O(1) space and O(n) time complexity)
+
 DOC
 require 'byebug'
 
